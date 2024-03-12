b7acec6ab0eb44a56d09916b42f13f5d8042c680
FazluLKarim4122 <md.fazlulkarim4122@gmail..com>
2024-03-12 20:14:16 +0600

    fetching blogs.json and load data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 
 import './App.css'
+import Header from './Components/Header/Header'
+import Blogs from './Components/Blogs/Blogs'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +10,8 @@ function App() {
   return (
     <>
       
-      <h1 className='text-4xl bg-red-300'>Knowledge Cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
