e22b48d520c97fbab1da0f5f893bf1e7322afc91
FazluLKarim4122 <md.fazlulkarim4122@gmail..com>
2024-03-13 14:52:19 +0600

    Cover Picture and title showed in blog section

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types';
 const Blog = ({blog}) => {
     console.log(blog)
+    const {title, cover, author, author_img, posted_date, reading_time, hashtags }= blog;
     return (
         <div>
-            
+            <img src={cover} alt={`Cover picture of the title ${title}`} />
+            <h2 className='text-4xl'>{title}</h2>
         </div>
     );
 };
--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
     const [blogs,setBlogs]=useState([])
@@ -7,9 +8,13 @@ const Blogs = () => {
         .then(res => res.json())
         .then(data => setBlogs(data))
     },[])
+
     return (
         <div className="w-2/3">
            <h2>Blogs: {blogs.length}</h2> 
+        {
+            blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+        }
         </div>
     );
 };
